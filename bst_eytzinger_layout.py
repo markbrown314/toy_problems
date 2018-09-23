47fab48ca6f672da57d2beec533524a248e49a40
Mark F. Brown <mark.brown314@gmail.com>
2018-09-23 08:19:18 -0400

    Improvements to BST takes in list, grows, and has search routine

--- bst_eytzinger_layout.py
+++ bst_eytzinger_layout.py
@@ -1,8 +1,9 @@
+# BST implementation using Eytzinger Layout
 class binary_search_tree:
-    def __init__(self, size):
-        if size <= 0:
-            return None
-        self.node_list = [None for x in range(size)]
+    def __init__(self, elements):
+        if not isinstance(elements, list):
+            raise TypeError("input not a list")
+        self.node_list = elements
     def len(self):
         return len(self.node_list)
     def right_child(self, pos):
@@ -16,21 +17,40 @@ class binary_search_tree:
             return self.left_child(current_pos)
         else: 
             return self.right_child(current_pos)
+    def grow(self, pos):
+        if pos < len(self.node_list):
+            return False
+        add_list = [ None for x in range((pos - len(self.node_list))+1)]
+        self.node_list += add_list
+        return True
     def insert(self, value):
         pos = 0
         while self.node_list[pos] != None:
             pos = self.next_pos(pos, value)
             if pos >= self.len():
-                return False
+                self.grow(pos)
         self.node_list[pos] = value
         return True
-        
-bst = binary_search_tree(10)
-bst.insert(8)
-bst.insert(4)
-bst.insert(12)
-bst.insert(2)
+    def search(self, value):
+        pos = 0
+        it = 0 # debug iteration count
+        while value != self.node_list[pos]:
+            it += 1
+            pos = self.next_pos(pos, value)
+            if pos >= self.len() or self.node_list[pos] == None:
+                return None
+        print ("iterations", it) # debug
+        return self.node_list[pos]
+
+# Test code
+bst = binary_search_tree([8,4,12,2])
 bst.insert(6)
 bst.insert(10)
 bst.insert(14)
+
+print(bst.search(8))
+print(bst.search(4))
+print(bst.search(14))
+print(bst.search(3.14))
+
 print(bst.node_list)
