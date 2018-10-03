2e55b72de1ee89822928c08ede8d0cbe9b9fa44a
Mark F. Brown <mark.brown314@gmail.com>
2018-10-03 13:23:20 -0400

    Cleaned up permute's input parameters

--- permutation.py
+++ permutation.py
@@ -1,10 +1,11 @@
-def permute(a, b):
-    if len(b) == 1:
-        yield(a+b)
-    for i in range(0, len(b)):
-        for j in permute(a + b[i], b[:i] + b[i+1:]):
-            yield j
-
+# provide all permutations of a string
+def permute(rem, fixed = ""):
+    if len(rem) == 1:
+        yield(fixed + rem)
+    for i in range(0, len(rem)):
+        for string in permute(rem[:i] + rem[i+1:], fixed + rem[i]):
+            yield string
+    
 if __name__ == "__main__":
-    for i in permute("", "abcd"):
-        print(i)
+    for i, string in enumerate(permute("abc")):
+        print(i+1, string)
