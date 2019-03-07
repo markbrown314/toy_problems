18f62b03824edc16a5db021a4578d21eed590b3f
Mark F. Brown <mark.brown314@gmail.com>
2019-03-06 20:26:55 -0500

    Fixed Problem 11

--- euler_problem11.py
+++ euler_problem11.py
@@ -8,10 +8,13 @@ def solve():
     selections.append([[0,0],[0,1],[0,2],[0,3]])
     selections.append([[0,0],[1,1],[2,2],[3,3]])
     selections.append([[0,0],[1,-1],[2,-2],[3,-3]])
-    number_grid = Grid("problem11.txt", 20, 20)
+    number_grid = Grid("problem11.txt", 20, 20, 2)
     #number_grid.pretty_print()
     for x in range(0,20):
         for y in range(0,20):
             for selection in selections:
                 results.append(reduce(lambda x,y: x*y, number_grid.select_items(selection, [x,y])))
     print(list(reversed(sorted(results))))
+
+if __name__ == "__main__":
+    solve()
