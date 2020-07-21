9549228ace5d203a83f4f248c9fc2b556d23c34a
Mark F. Brown <mark.brown314@gmail.com>
2020-07-21 05:13:30 -0400

    Checking in current work

--- Coding Problems/Algorithm Design Skiena/1_1_robot_tour_optimization.py
+++ Coding Problems/Algorithm Design Skiena/1_1_robot_tour_optimization.py
@@ -5,7 +5,7 @@ Output: What is the shortest cycle tour that visits each point in set S?
 """
 import random
 import math
-num_points = 3
+num_points = 10
 max_x = 100
 max_y = 100
 S = set()
@@ -15,49 +15,19 @@ def euclidian_distance(a, b):
         return 0
     return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
 
-def build_adj_matrix(matrix, a, b):
-    if matrix == None:
-        matrix = {}
-    if a in matrix:
-        a_dict = matrix[a]
+def build_adj_list(adj_list, a, b):
+    if adj_list == None:
+        adj_list = {}
+    if a in adj_list:
+        a_dict = adj_list[a]
     else:
-        matrix[a] = {}
-        a_dict = matrix[a]
-        a_dict["_closest_point"] = None
+        adj_list[a] = {}
+        a_dict = adj_list[a]
 
     a_dict[b] = euclidian_distance(a,b)
 
-    if a == b:
-        return matrix
+    return adj_list
 
-    if a_dict["_closest_point"] == None:
-        a_dict["_closest_point"] = b
-        return matrix
-
-    if euclidian_distance(a, a_dict["_closest_point"]) > a_dict[b]:
-        a_dict["_closest_point"] = b
-
-    return matrix
-
-def closest_point_adj_matrix(matrix, a):
-    if a in matrix:
-        a_dict = matrix[a]
-    else:
-        raise(KeyError("content " + a + " not in adj matrix"))
-    return a_dict["_closest_point"]
-
-def lookup_adj_matrix(matrix, a, b):
-    if a in matrix:
-        a_dict = matrix[a]
-    else:
-        raise(KeyError("content " + a + " not in adj matrix"))
-    
-    if b in a_dict:
-        raise(KeyError("content " + b + " not mapped to " + a +" in adj matrix"))
-
-    return(a_dict[b])
-
-"""
 # generate random set of points
 for i in range(0, num_points):
     while True:
@@ -66,19 +36,22 @@ for i in range(0, num_points):
         if (x,y) not in S:
             break
     S.add((x,y))
+    
 """
 S = set([(1,3),(2,7),(3,5)])
+"""
 
-matrix = None
+adj_list = None
 
-# build matrix
+# build adjacency list
 for p1 in S:
     for p2 in S:
-        matrix = build_adj_matrix(matrix, p1, p2)
+        adj_list = build_adj_list(adj_list, p1, p2)
 
 print("points", S)
-print("adj matrix", matrix)
+print("adjacency list", adj_list)
 visited = {}
+tour = []
 
 # build a visit map
 for p in S:
@@ -89,6 +62,25 @@ visit_count = 1
 p = list(S)[0]
 visited[p] = True
 
-#while visit_count < len(S):
-for p in S:
-    print("closest dist for", p, closest_point_adj_matrix(matrix, p))
+while True:
+    print(p, end="")
+    if visit_count >= len(S):
+        break
+    print(" -> ", end="")
+    # sort adjacency list
+    path = []
+    v = adj_list[p]
+    for key in v:
+        path.append([key, v[key]])
+    sorted(path, key = lambda p: p[1])
+    for neighb in path:
+        np = None
+        #print (neighb[0], visited[neighb[0]])
+        if not visited[neighb[0]]:
+            np = neighb[0]
+            break
+    if not np:
+        raise(LookupError("Cannot find unvisited neighbor!"))
+    visited[np] = True
+    p = np
+    visit_count += 1    
